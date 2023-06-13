36354b6894b62f1a5d23f1e99df0dca2eb3eab19
J-Ciro <37349818+J-Ciro@users.noreply.github.com>
2023-06-13 13:19:44 -0500

    Cambio para axios

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,17 +1,17 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./SearchBar.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
-const SearchBar = ({setResults}) => {
-
+const SearchBar = ({ setResults }) => {
   const [input, setInput] = useState("");
 
   const fetchData = (value) => {
-    fetch("https://pizza-api-production.up.railway.app/pizzas")
-      .then((response) => response.json())
-      .then((json) => {
-        const results = json.filter((pizza) => {
+    axios
+      .get("https://pizza-api-production.up.railway.app/pizzas")
+      .then((response) => {
+        const results = response.data.filter((pizza) => {
           return (
             value &&
             pizza &&
@@ -20,21 +20,29 @@ const SearchBar = ({setResults}) => {
           );
         });
         setResults(results);
+      })
+      .catch((error) => {
+        console.error(error);
       });
   };
+
   const handleChange = (value) => {
     setInput(value);
     fetchData(value);
   };
 
-
   return (
     <>
       <div className="search">
-        <input type="text" className="input" placeholder="Pizza de pepperoni, mexicana, hawaiana..." value={input}
-        onChange={(e) => handleChange(e.target.value)} />
+        <input
+          type="text"
+          className="input"
+          placeholder="Pizza de pepperoni, mexicana, hawaiana..."
+          value={input}
+          onChange={(e) => handleChange(e.target.value)}
+        />
         <button type="submit">
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
       </div>
     </>
